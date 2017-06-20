336bbdda316a1518e4cedbe2e715fd5fd76d7f4b
Sirui Sun <Sirui.sun1@gmail.com>
2017-06-20 14:31:44 -0700

    fit-and-finish updates to sample

--- manageRefresh.ps1
+++ manageRefresh.ps1
@@ -1,11 +1,7 @@
 # This sample script calls the Power BI API to progammtically trigger a refresh for the dataset
 # It then calls the Power BI API to progammatically to get the refresh history for that dataset
-
-# Instructions:
-# 1. Set up a dataset for refresh in the Power BI service - make sure that the dataset can be 
-# updated successfully
-# 2. Fill in the parameters below
-# 3. Run the PowerShell script
+# For full documentation on the REST APIs, see:
+# https://msdn.microsoft.com/en-us/library/mt203551.aspx 
 
 # Parameters - fill these in before running the script!
 # =====================================================
@@ -21,28 +17,12 @@ $datasetID = " FILL ME IN " # the ID of the dataset that hosts the dataset
 # AAD Client ID
 # To get this, go to the following page and follow the steps to provision an app
 # https://dev.powerbi.com/apps
+
 # To get the sample to work, ensure that you have the following fields:
 # App Type: Native app
 # Redirect URL: urn:ietf:wg:oauth:2.0:oob
 #  Level of access: all dataset APIs
 $clientId = " FILL ME IN " 
-
-# Get the auth token from AAD
-$token = GetAuthToken
-
-# Building Rest API header with authorization token
-$authHeader = @{
-   'Content-Type'='application\json'
-   'Authorization'=$token.CreateAuthorizationHeader()
-}
-
-# Refresh the dataset
-$uri = "https://api.powerbi.com/v1.0/myorg/groups/$groupID/datasets/$datasetID/refreshes"
-Invoke-RestMethod -Uri $uri –Headers $authHeader –Method POST –Verbose
-
-# Check the refresh history
-$uri = "https://api.powerbi.com/v1.0/myorg/groups/86fa28d7-a393-478a-80ba-e822b122d94f/datasets/cdc86969-5b67-4363-9df9-2900e2514c6f/refreshes"
-Invoke-RestMethod -Uri $uri –Headers $authHeader –Method GET –Verbose
 
 # Calls the Active Directory Authentication Library (ADAL) to authenticate against AAD
 function GetAuthToken
@@ -63,7 +43,32 @@ function GetAuthToken
  
        $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
  
-       $authResult = $authContext.AcquireToken($resourceAppIdURI, $clientId, $redirectUri, "Auto")
+       $authResult = $authContext.AcquireToken($resourceAppIdURI, $clientId, $redirectUri, "Always")
  
        return $authResult
 }
+
+# Get the auth token from AAD
+$token = GetAuthToken
+
+# Building Rest API header with authorization token
+$authHeader = @{
+   'Content-Type'='application\json'
+   'Authorization'=$token.CreateAuthorizationHeader()
+}
+
+# properly format groups path
+$groupsPath = ""
+if ($groupID -eq "me") {
+    $groupsPath = "myorg"
+} else {
+    $groupsPath = "myorg/groups/$groupID"
+}
+
+# Refresh the dataset
+$uri = "https://api.powerbi.com/v1.0/$groupsPath/datasets/$datasetID/refreshes"
+Invoke-RestMethod -Uri $uri –Headers $authHeader –Method POST –Verbose
+
+# Check the refresh history
+$uri = "https://api.powerbi.com/v1.0/$groupsPath/datasets/$datasetID/refreshes"
+Invoke-RestMethod -Uri $uri –Headers $authHeader –Method GET –Verbose
