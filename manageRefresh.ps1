16d74d7be4a265a583fe06a1ca551b2765419ff8
Jonathan Woodard <sqlsauce@users.noreply.github.com>
2019-10-31 19:07:27 -0700

    Update manageRefresh.ps1 to fix error
    In issue #5, it's pointed out in https://github.com/Azure-Samples/powerbi-powershell/issues/5#issuecomment-365752905 that the Invoke-RestMethod parameter names have long dashes, which causes problems. This changes the en dashes to dashes, which fixed the error.

--- manageRefresh.ps1
+++ manageRefresh.ps1
@@ -71,8 +71,8 @@ if ($groupID -eq "me") {
 
 # Refresh the dataset
 $uri = "https://api.powerbi.com/v1.0/$groupsPath/datasets/$datasetID/refreshes"
-Invoke-RestMethod -Uri $uri –Headers $authHeader –Method POST –Verbose
+Invoke-RestMethod -Uri $uri -Headers $authHeader -Method POST -Verbose
 
 # Check the refresh history
 $uri = "https://api.powerbi.com/v1.0/$groupsPath/datasets/$datasetID/refreshes"
-Invoke-RestMethod -Uri $uri –Headers $authHeader –Method GET –Verbose
+Invoke-RestMethod -Uri $uri -Headers $authHeader -Method GET -Verbose
